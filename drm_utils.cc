fe61bbeb066b1f49aa002e2afeab51576f637fcb
xueshen.an <xueshen.an@aeroratech.com>
2024-06-11 09:10:58 +0800

    add utils for calc bpp and height

---
+++ drm_utils.cc
@@ -0,0 +1,65 @@
+#include "drm_utils.h"
+
+#include <drm_fourcc.h>
+
+uint32_t drm_bpp_from_drm_format(uint32_t drm_format) {
+    uint32_t bpp = 0;
+    switch (drm_format) {
+        case DRM_FORMAT_YUV420:
+        case DRM_FORMAT_YVU420:
+        case DRM_FORMAT_YUV422:
+        case DRM_FORMAT_NV12:
+        case DRM_FORMAT_NV21:
+        case DRM_FORMAT_NV16:
+        case DRM_FORMAT_NV61:
+        case DRM_FORMAT_NV24:
+            bpp = 8;
+            break;
+        case DRM_FORMAT_P010:
+            bpp = 10;
+            break;
+        case DRM_FORMAT_UYVY:
+        case DRM_FORMAT_YUYV:
+        case DRM_FORMAT_YVYU:
+        case DRM_FORMAT_P016:
+        case DRM_FORMAT_RGB565:
+        case DRM_FORMAT_BGR565:
+            bpp = 16;
+            break;
+        case DRM_FORMAT_BGR888:
+        case DRM_FORMAT_RGB888:
+            bpp = 24;
+            break;
+        default:
+            bpp = 32;
+            break;
+    }
+    return bpp;
+}
+
+uint32_t drm_height_from_drm_format(uint32_t drm_format, uint32_t height) {
+    uint32_t ret = 0;
+    switch (drm_format) {
+        case DRM_FORMAT_YUV420:
+        case DRM_FORMAT_YVU420:
+        case DRM_FORMAT_YUV422:
+        case DRM_FORMAT_NV12: /* 2x2 subsampled Cr:Cb plane */
+        case DRM_FORMAT_NV21:
+        case DRM_FORMAT_P010:
+        case DRM_FORMAT_P016:
+            ret = height * 3 / 2;
+            break;
+        case DRM_FORMAT_NV16: /* 2x1 subsampled Cr:Cb plane */
+        case DRM_FORMAT_NV61:
+            ret = height * 2;
+            break;
+        case DRM_FORMAT_NV24: /* non-subsampled Cr:Cb plane */
+            ret = height * 3;
+            break;
+        default:
+            ret = height;
+            break;
+    }
+
+    return ret;
+}
---
+++ drm_utils.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdint.h>
+
+uint32_t drm_bpp_from_drm_format(uint32_t drm_format);
+
+uint32_t drm_height_from_drm_format(uint32_t drm_format, uint32_t height);
